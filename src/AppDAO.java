e2f38a6d8b0ddf7e7a9e83aed2a1a8fe23ea11f3
TheProtogen <mrprotogen97@protonmail.com>
2024-06-07 11:43:26 -0300

    Update 1

---
+++ src/AppDAO.java
@@ -0,0 +1,6 @@
+public interface AppDAO {
+    public void pesquisar();
+    public void criar();
+    public void atualizar();
+    public void deletar();
+}
---
+++ src/Vestido.java
@@ -0,0 +1,19 @@
+public class Vestido {
+    
+    public int id;
+    public int tamanho;
+    public String cor;
+    public String marca;
+    public String finalidade;
+    public double valor;
+    
+    public Vestido(int id, int tamanho, String cor, String marca, String finalidade, double valor) {
+        this.id = id;
+        this.tamanho = tamanho;
+        this.cor = cor;
+        this.marca = marca;
+        this.finalidade = finalidade;
+        this.valor = valor;
+    }
+
+}
